864853a3f2aac677d317103d3947c776727a0975
Danylo Rybchynskyi <d.ribchinski@gmail.com>
2025-04-29 18:34:18 +0300

    fix: invalid comments

--- create_roles.sql
+++ create_roles.sql
@@ -3,20 +3,20 @@ CREATE ROLE IF NOT EXISTS trainer_role;
 CREATE ROLE IF NOT EXISTS manager_role;
 CREATE ROLE IF NOT EXISTS director_role;
 
---employee
+-- employee
 GRANT SELECT ON *.* TO employee_role;
 
---trainer
+-- trainer
 GRANT SELECT ON *.* TO trainer_role;
 GRANT INSERT, UPDATE, DELETE ON Train TO trainer_role;
 
---manager
+-- manager
 GRANT SELECT ON *.* TO manager_role;
 GRANT INSERT, UPDATE, DELETE ON Schedule TO manager_role;
 GRANT INSERT, UPDATE, DELETE ON Manages TO manager_role;
 GRANT INSERT, UPDATE, DELETE ON TrainedIn TO manager_role;
 
---director
+-- director
 GRANT SELECT ON *.* TO director_role;
 GRANT INSERT, UPDATE, DELETE ON Schedule TO director_role;
 GRANT INSERT, UPDATE, DELETE ON Train TO director_role;
@@ -26,7 +26,7 @@ GRANT INSERT, UPDATE, DELETE ON Manages TO director_role;
 GRANT INSERT, UPDATE, DELETE ON TrainedIn TO director_role;
 
 
---create users
+-- create users
 CREATE USER IF NOT EXISTS 'employee_user'@'%' IDENTIFIED BY '123';
 GRANT employee_role TO 'employee_user'@'%';
 
@@ -39,6 +39,6 @@ GRANT manager_role TO 'manager_user'@'%';
 CREATE USER IF NOT EXISTS 'director_user'@'%' IDENTIFIED BY '123456789';
 GRANT director_role TO 'director_user'@'%';
 
---remove online root login
+-- remove online root login
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 FLUSH PRIVILEGES;
