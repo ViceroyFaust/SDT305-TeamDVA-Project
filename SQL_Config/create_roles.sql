384a98a38409689ed6e7931814e47211f17d476d
Vladyslav <vladislavshvetsua@gmail.com>
2025-05-08 16:29:25 +0200

    chore: Added mariadb provider and adjusted roles queries to MariaDb

--- SQL_Config/create_roles.sql
+++ SQL_Config/create_roles.sql
@@ -28,20 +28,21 @@ GRANT INSERT, UPDATE, DELETE ON TrainedIn TO director_role;
 
 -- create users
 CREATE USER IF NOT EXISTS 'employee'@'%' IDENTIFIED BY '123';
-GRANT employee_role to 'employee'@'%';
-SET DEFAULT ROLE employee_role to 'employee'@'%';
+GRANT employee_role TO 'employee'@'%';
+SET DEFAULT ROLE employee_role FOR 'employee'@'%';
 
 CREATE USER IF NOT EXISTS 'trainer'@'%' IDENTIFIED BY '12345';
-GRANT trainer_role to 'trainer'@'%';
-SET DEFAULT ROLE trainer_role to 'trainer'@'%';
+GRANT trainer_role TO 'trainer'@'%';
+SET DEFAULT ROLE trainer_role FOR 'trainer'@'%';
 
 CREATE USER IF NOT EXISTS 'manager'@'%' IDENTIFIED BY '123456';
-GRANT manager_role to 'manager'@'%';
-SET DEFAULT ROLE manager_role to 'manager'@'%';
+GRANT manager_role TO 'manager'@'%';
+SET DEFAULT ROLE manager_role FOR 'manager'@'%';
+
 
 CREATE USER IF NOT EXISTS 'director'@'%' IDENTIFIED BY '123456789';
-GRANT director_role to 'director'@'%';
-SET DEFAULT ROLE director_role to 'director'@'%';
+GRANT director_role TO 'director'@'%';
+SET DEFAULT ROLE director_role FOR 'director'@'%';
 
 -- remove online root login
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
